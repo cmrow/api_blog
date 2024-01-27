421e5f57b3b4beed0856e0ad62f0987bf919fcfc
Carlos Mario Rodríguez <carlosmario.rodriguez@outlook.com>
2024-01-26 21:14:23 -0500

    Added dotenv as a dependency and used it to load environment variables. Connected to the database in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const dbConnect = require("./config/db/dbConnect");
 const app = express();
 
+dbConnect();
 //server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running ${PORT}`));
